b9b0e1f7b0acce1fddaac993dbacb1d707f2e262
AvelFir <guichasemartinsgmr@hotmail.com>
2021-12-13 16:10:49 -0300

    commit inicial, primeiro projeto spring batch

---
+++ primeiro_projeto_spring_batch/src/main/java/br/com/guilherme/primeiro_projeto_spring_batch/PrimeiroProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.primeiro_projeto_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringBatchApplication.class, args);
+	}
+
+}
---
+++ primeiro_projeto_spring_batch/src/test/java/br/com/guilherme/primeiro_projeto_spring_batch/PrimeiroProjetoSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.primeiro_projeto_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
