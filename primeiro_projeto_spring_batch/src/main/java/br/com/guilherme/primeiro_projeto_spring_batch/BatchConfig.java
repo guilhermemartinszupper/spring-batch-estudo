fc2d769c41f58c1344ee4f2f9c3bf194a3579cb2
AvelFir <guichasemartinsgmr@hotmail.com>
2021-12-13 18:02:37 -0300

    feito job simples com tasklet

---
+++ primeiro_projeto_spring_batch/src/main/java/br/com/guilherme/primeiro_projeto_spring_batch/BatchConfig.java
@@ -0,0 +1,56 @@
+package br.com.guilherme.primeiro_projeto_spring_batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job imprimeOlaJob(){
+        return jobBuilderFactory
+                .get("imprimeOlaJob")
+                .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step imprimeOlaStep() {
+        return stepBuilderFactory
+                .get("imprimeOlaStep")
+                .tasklet(imprimeOlaTasklet(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}")String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Ola, " + nome);
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
